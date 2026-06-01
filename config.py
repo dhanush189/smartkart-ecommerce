1adcb013438ee3c0068d375a1e168b559559f501
dhanush <dhanushnemmoju@gmail.com>
2026-06-01 12:31:19 +0530

    Move secrets to environment variables

--- config.py
+++ config.py
@@ -5,22 +5,21 @@
 # details, Email settings, Razorpay keys etc.
 # ------------------------------------
 
-SECRET_KEY = "your_secret_key_here"   # used for sessions
+import os
 
-# Super Admin / Owner Login
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 SUPERADMIN_EMAIL = "owner@smartkart.com"
 SUPERADMIN_PASSWORD = "owner123"
 
-# SQLite Database Configuration
 DB_PATH = "smartkart.db"
 
-# Email SMTP Settings
 MAIL_SERVER = 'smtp.gmail.com'
 MAIL_PORT = 587
 MAIL_USE_TLS = True
-MAIL_USERNAME = 'dhanushnemmoju@gmail.com'
-MAIL_PASSWORD = 'fvlv pftt vetd jbko'   # Gmail App Password
 
-# Razorpay API Keys
-RAZORPAY_KEY_ID = 'rzp_test_StVWmxr1lP0kaM'
-RAZORPAY_KEY_SECRET = 'A8ua2gUN1XkbCImFkd8MPJx0'
+MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+RAZORPAY_KEY_ID = os.environ.get("RAZORPAY_KEY_ID")
+RAZORPAY_KEY_SECRET = os.environ.get("RAZORPAY_KEY_SECRET")
